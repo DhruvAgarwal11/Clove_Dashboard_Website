c39e1e2a4209c4903c08cccbdf8a67686373719d
Dhruv Agarwal <dhruvagarwal@gmail.com>
2022-08-13 17:59:36 -0700

    removing cors

--- docs/script.js
+++ docs/script.js
@@ -844,7 +844,7 @@ function retrieveCustomerPaymentMethod(paymentMethodId) {
 function getConfig() {
   var urlAWSgetConfig = urlAWS + '&typeOfRequest=config';
   console.log(urlAWSgetConfig);
-  return fetch(urlAWSgetConfig, {mode: "cors", method: "get",  headers: headers}).then((response) => {
+  return fetch(urlAWSgetConfig, {method: "get",  headers: headers}).then((response) => {
     console.log(response);
     return response.json(); 
     //stripeElements(result.publishableKey);
